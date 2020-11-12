e1ad23dced9f3db6f2f027bfb6c30fec3594a635
ADryInkCartridge <bayuaaas@gmail.com>
2020-11-13 02:09:37 +0700

    Added Searchbar, Fixed Button

--- lib/Card/HomeCard.dart
+++ lib/Card/HomeCard.dart
@@ -40,82 +40,107 @@ Widget subjectList(BuildContext context) {
   return ListView.builder(
     itemCount: items.length,
     itemBuilder: (context, index) {
-      return Card(
-        margin: EdgeInsets.fromLTRB(30, 15, 20, 15),
-        shape: RoundedRectangleBorder(
-          side: BorderSide(color: Colors.orange, width: 1),
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        child: new InkWell(
-          onTap: () => print("tapped"),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Expanded(
-                  flex: 2,
-                  child: Container(
-                    padding: EdgeInsets.fromLTRB(20, 10, 0, 10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          items[index].pelajaran,
-                          style: TextStyle(
-                            fontFamily: "Poppins",
-                            fontWeight: FontWeight.w500,
-                            fontSize: 15,
-                          ),
+      return Container(
+        child: Stack(
+          children: [
+            Card(
+              margin: EdgeInsets.fromLTRB(30, 15, 20, 15),
+              shape: RoundedRectangleBorder(
+                side: BorderSide(color: Colors.orange, width: 1),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                      flex: 2,
+                      child: Container(
+                        padding: EdgeInsets.fromLTRB(20, 10, 0, 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              items[index].pelajaran,
+                              style: TextStyle(
+                                fontFamily: "Poppins",
+                                fontWeight: FontWeight.w500,
+                                fontSize: 15,
+                              ),
+                            ),
+                            Text(
+                              "Materi : ${items[index].materi}",
+                              style: TextStyle(
+                                height: 1.8,
+                                fontFamily: "Poppins",
+                                fontWeight: FontWeight.w700,
+                                fontSize: 12,
+                              ),
+                            ),
+                            Text(
+                              items[index].nama,
+                              style: TextStyle(
+                                height: 1.8,
+                                fontFamily: "Poppins",
+                                fontWeight: FontWeight.w500,
+                                fontSize: 12,
+                              ),
+                            ),
+                          ],
                         ),
-                        Text(
-                          "Materi : ${items[index].materi}",
-                          style: TextStyle(
-                            height: 1.8,
-                            fontFamily: "Poppins",
-                            fontWeight: FontWeight.w700,
-                            fontSize: 12,
-                          ),
+                      )),
+                  Expanded(
+                      flex: 1,
+                      child: Container(
+                        padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: <Widget>[
+                            Text(
+                              "Post",
+                              style: TextStyle(
+                                fontFamily: "Poppins",
+                                fontWeight: FontWeight.w500,
+                                fontSize: 18,
+                              ),
+                            ),
+                            Text(
+                              items[index].jam.toStringAsFixed(2),
+                              textAlign: TextAlign.right,
+                              style: TextStyle(
+                                fontFamily: "Poppins",
+                                fontWeight: FontWeight.w500,
+                                fontSize: 26,
+                              ),
+                            ),
+                          ],
                         ),
-                        Text(
-                          items[index].nama,
-                          style: TextStyle(
-                            height: 1.8,
-                            fontFamily: "Poppins",
-                            fontWeight: FontWeight.w500,
-                            fontSize: 12,
-                          ),
-                        ),
-                      ],
-                    ),
-                  )),
-              Expanded(
-                  flex: 1,
-                  child: Container(
-                    padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: <Widget>[
-                        Text(
-                          "Post",
-                          style: TextStyle(
-                            fontFamily: "Poppins",
-                            fontWeight: FontWeight.w500,
-                            fontSize: 18,
-                          ),
-                        ),
-                        Text(
-                          items[index].jam.toStringAsFixed(2),
-                          textAlign: TextAlign.right,
-                          style: TextStyle(
-                            fontFamily: "Poppins",
-                            fontWeight: FontWeight.w500,
-                            fontSize: 26,
-                          ),
-                        ),
-                      ],
-                    ),
-                  )),
-            ],
-          ),
+                      )),
+                ],
+              ),
+            ),
+            Positioned(
+              top: 89.5,
+              right: 80,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 2,
+                          spreadRadius: 1,
+                          offset: Offset(0.0, 3)),
+                    ]),
+                child: ClipOval(
+                  child: Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                          onTap: () {}, child: Icon(Icons.navigate_next))),
+                ),
+              ),
+            ),
+          ],
         ),
       );
     },
--- lib/Home.dart
+++ lib/Home.dart
@@ -28,80 +28,129 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final searchBar = TextEditingController();
+  bool barEmpty = true;
   int id = 1;
   String name = "Melanchton Bonaficio Butarbutar";
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+        resizeToAvoidBottomInset: false,
         body: SafeArea(
-      child: Stack(
-        children: [
-          Container(
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-            image: AssetImage("assets/img/bulet2 3.png"),
-            fit: BoxFit.cover,
-          ))),
-          AppBar(
-            leading: IconButton(
-              icon: Icon(Icons.arrow_back, color: Colors.black),
-              onPressed: () => Navigator.of(context).pop(),
-            ),
-            backgroundColor: Colors.transparent,
-            elevation: 0.0,
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Image.asset(
-                  "assets/img/logo.png",
-                  width: 140,
-                  height: 400,
+          child: Stack(
+            children: [
+              Container(
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                image: AssetImage("assets/img/bulet2 3.png"),
+                fit: BoxFit.cover,
+              ))),
+              AppBar(
+                leading: IconButton(
+                  icon: Icon(Icons.arrow_back, color: Colors.black),
+                  onPressed: () => Navigator.of(context).pop(),
                 ),
-              ],
-            ),
-          ),
-          Column(
-            children: <Widget>[
-              SizedBox(
-                height: 75,
+                backgroundColor: Colors.transparent,
+                elevation: 0.0,
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(18.0),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.grey,
+                                blurRadius: 2,
+                                spreadRadius: 1,
+                                offset: Offset(0.0, 3)),
+                          ]),
+                      height: 30,
+                      width: 100,
+                      child: Center(
+                        child: TextFormField(
+                          style: TextStyle(fontFamily: "Poppins", fontSize: 14),
+                          textAlign: TextAlign.center,
+                          cursorColor: Colors.grey,
+                          decoration: new InputDecoration(
+                            contentPadding: EdgeInsets.only(
+                              bottom: 0,
+                            ),
+                            hintText: "Search",
+                            hintStyle:
+                                TextStyle(fontFamily: "Poppins", fontSize: 14),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              borderSide: BorderSide(
+                                color: Colors.transparent,
+                              ),
+                            ),
+                            enabledBorder: new OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              borderSide: BorderSide(
+                                color: Colors.transparent,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              Center(
-                child: GestureDetector(
-                  onTap: () => Navigator.pop(context),
-                  child: CircleAvatar(
-                    backgroundImage: AssetImage("assets/img/mel.PNG"),
-                    radius: 60,
+              Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 50,
                   ),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Center(
-                child: status(id),
-              ),
-              Center(
-                child: Text(
-                  name,
-                  style: TextStyle(
-                    fontFamily: "Poppins",
-                    fontWeight: FontWeight.w500,
-                    fontSize: 18,
+                  Center(
+                    child: GestureDetector(
+                      onTap: () => Navigator.pushReplacementNamed(context, '/'),
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage("assets/img/mel.PNG"),
+                        radius: 60,
+                      ),
+                    ),
                   ),
-                ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Center(
+                    child: status(id),
+                  ),
+                  Center(
+                    child: Text(
+                      name,
+                      style: TextStyle(
+                        fontFamily: "Poppins",
+                        fontWeight: FontWeight.w500,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Center(
+                    child: Text(
+                      "Questions",
+                      style: TextStyle(
+                        fontFamily: "Poppins",
+                        fontWeight: FontWeight.w800,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: SubjectCard(),
+                    ),
+                  )
+                ],
               ),
-              SizedBox(
-                height: 15,
-              ),
-              Expanded(
-                child: Center(
-                  child: SubjectCard(),
-                ),
-              )
             ],
           ),
-        ],
-      ),
-    ));
+        ));
   }
 }
