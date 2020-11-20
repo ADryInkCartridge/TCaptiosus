38284a49e2cde7f963e32f64444b253bee6078c3
ADryInkCartridge <bayuaaas@gmail.com>
2020-11-20 22:55:26 +0700

    ehe

--- lib/Login/signup.dart
+++ lib/Login/signup.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:forum_test/Services/auth.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 void main() => runApp(MaterialApp(
@@ -12,9 +13,13 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+  String name = '';
   String email = '';
   String password = '';
   String _value;
+  bool status = true;
+  final authService auth = authService();
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     final node = FocusScope.of(context);
@@ -32,176 +37,204 @@ class _SignUpState extends State<SignUp> {
             children: <Widget>[
               Padding(
                 padding: EdgeInsets.fromLTRB(40.0, 155.0, 40.0, 0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Container(
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage("assets/img/bg.png"),
-                          fit: BoxFit.cover,
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage("assets/img/bg.png"),
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
-                    ),
-                    Image.asset(
-                      "assets/img/logo.png",
-                    ),
-                    SizedBox(height: 40.0),
-                    TextFormField(
-                      // color: Colors.orange,
-                      cursorColor: HexColor("ef9c80"),
-                      decoration: new InputDecoration(
-                        labelText: "Name",
-                        hintText: "Enter your name",
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                          borderSide: BorderSide(
-                            color: HexColor("ef9c80"),
-                          ),
-                        ),
-                        enabledBorder: new OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                          borderSide: BorderSide(
-                            color: HexColor("ef9c80"),
-                            width: 2.0,
-                          ),
-                          // labelText: "Email,
-                        ),
+                      Image.asset(
+                        "assets/img/logo.png",
                       ),
-                      textInputAction: TextInputAction.next,
-                      onEditingComplete: () => node.nextFocus(),
-                    ),
-                    SizedBox(height: 30.0),
-                    TextFormField(
-                      // color: Colors.orange,
-                      cursorColor: HexColor("ef9c80"),
-                      decoration: new InputDecoration(
-                        labelText: "Email",
-                        hintText: "Enter your email",
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                          borderSide: BorderSide(
-                            color: HexColor("ef9c80"),
-                          ),
-                        ),
-                        enabledBorder: new OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                          borderSide: BorderSide(
-                            color: HexColor("ef9c80"),
-                            width: 2.0,
-                          ),
-                          // labelText: "Email,
-                        ),
-                      ),
-                      textInputAction: TextInputAction.next,
-                      onEditingComplete: () => node.nextFocus(),
-                    ),
-                    SizedBox(height: 30.0),
-                    TextFormField(
-                      // color: Colors.orange,
-                      cursorColor: HexColor("ef9c80"),
-                      decoration: new InputDecoration(
-                        labelText: "Password",
-                        hintText: "Enter your password",
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                          borderSide: BorderSide(
-                            color: HexColor("ef9c80"),
-                          ),
-                        ),
-                        enabledBorder: new OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                          borderSide: BorderSide(
-                            color: HexColor("ef9c80"),
-                            width: 2.0,
-                          ),
-                          // labelText: "Email,
-                        ),
-                      ),
-                      textInputAction: TextInputAction.next,
-                      onEditingComplete: () => node.nextFocus(),
-                    ),
-                    SizedBox(height: 30.0),
-                    // TextFormField(
-                    //   // color: Colors.orange,
-                    //   cursorColor: HexColor("ef9c80"),
-                    //   decoration: new InputDecoration(
-                    //     labelText: "Title",
-                    //     hintText: "Enter your title",
-                    //     focusedBorder: OutlineInputBorder(
-                    //       borderRadius: BorderRadius.circular(10.0),
-                    //       borderSide: BorderSide(
-                    //         color: HexColor("ef9c80"),
-                    //       ),
-                    //     ),
-                    //     enabledBorder: new OutlineInputBorder(
-                    //       borderRadius: BorderRadius.circular(10.0),
-                    //       borderSide: BorderSide(
-                    //         color: HexColor("ef9c80"),
-                    //         width: 2.0,
-                    //       ),
-                    //       // labelText: "Email,
-                    //     ),
-                    //   ),
-                    //   textInputAction: TextInputAction.next,
-                    //   onEditingComplete: () => node.nextFocus(),
-                    // ),
-                    Container(
-                      child: DropdownButton<String>(
-                        style: TextStyle(
-                          fontFamily: 'Poppins',
-                        ),
-                        items: [
-                          DropdownMenuItem<String>(
-                            child: Text(
-                              "Dosen",
-                              style: TextStyle(
-                                fontFamily: 'Poppins',
-                                fontSize: 15,
-                                color: Colors.black,
-                              ),
-                            ),
-                            value: 'one',
-                          ),
-                          DropdownMenuItem<String>(
-                            child: Text(
-                              "Mahasiswa",
-                              style: TextStyle(
-                                fontFamily: 'Poppins',
-                                fontSize: 15,
-                                color: Colors.black,
-                              ),
-                            ),
-                            value: 'two',
-                          ),
-                        ],
-                        onChanged: (String value) {
-                          setState(() {
-                            _value = value;
-                          });
+                      SizedBox(height: 40.0),
+                      TextFormField(
+                        // color: Colors.orange,
+                        onChanged: (val) {
+                          setState(() => name = val);
                         },
-                        hint: Text('Title'),
-                        value: _value,
-                      ),
-                    ),
-                    SizedBox(height: 30.0),
-                    Center(
-                      child: RaisedButton(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
+                        cursorColor: HexColor("ef9c80"),
+                        decoration: new InputDecoration(
+                          labelText: "Name",
+                          hintText: "Enter your name",
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: BorderSide(
+                              color: HexColor("ef9c80"),
+                            ),
+                          ),
+                          enabledBorder: new OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: BorderSide(
+                              color: HexColor("ef9c80"),
+                              width: 2.0,
+                            ),
+                            // labelText: "Email,
+                          ),
                         ),
-                        color: HexColor("8293c2"),
-                        child: Text(
-                          "Sign Up",
+                        textInputAction: TextInputAction.next,
+                        onEditingComplete: () => node.nextFocus(),
+                      ),
+                      SizedBox(height: 30.0),
+                      TextFormField(
+                        // color: Colors.orange,
+                        onChanged: (val) {
+                          setState(() => email = val);
+                        },
+                        cursorColor: HexColor("ef9c80"),
+                        decoration: new InputDecoration(
+                          labelText: "Email",
+                          hintText: "Enter your email",
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: BorderSide(
+                              color: HexColor("ef9c80"),
+                            ),
+                          ),
+                          enabledBorder: new OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: BorderSide(
+                              color: HexColor("ef9c80"),
+                              width: 2.0,
+                            ),
+                            // labelText: "Email,
+                          ),
+                        ),
+                        textInputAction: TextInputAction.next,
+                        onEditingComplete: () => node.nextFocus(),
+                      ),
+                      SizedBox(height: 30.0),
+                      TextFormField(
+                        // color: Colors.orange,
+                        onChanged: (val) {
+                          setState(() => password = val);
+                        },
+                        cursorColor: HexColor("ef9c80"),
+                        decoration: new InputDecoration(
+                          labelText: "Password",
+                          hintText: "Enter your password",
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: BorderSide(
+                              color: HexColor("ef9c80"),
+                            ),
+                          ),
+                          enabledBorder: new OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: BorderSide(
+                              color: HexColor("ef9c80"),
+                              width: 2.0,
+                            ),
+                            // labelText: "Email,
+                          ),
+                        ),
+                        textInputAction: TextInputAction.next,
+                        onEditingComplete: () => node.nextFocus(),
+                      ),
+                      SizedBox(height: 30.0),
+                      // TextFormField(
+                      //   // color: Colors.orange,
+                      //   cursorColor: HexColor("ef9c80"),
+                      //   decoration: new InputDecoration(
+                      //     labelText: "Title",
+                      //     hintText: "Enter your title",
+                      //     focusedBorder: OutlineInputBorder(
+                      //       borderRadius: BorderRadius.circular(10.0),
+                      //       borderSide: BorderSide(
+                      //         color: HexColor("ef9c80"),
+                      //       ),
+                      //     ),
+                      //     enabledBorder: new OutlineInputBorder(
+                      //       borderRadius: BorderRadius.circular(10.0),
+                      //       borderSide: BorderSide(
+                      //         color: HexColor("ef9c80"),
+                      //         width: 2.0,
+                      //       ),
+                      //       // labelText: "Email,
+                      //     ),
+                      //   ),
+                      //   textInputAction: TextInputAction.next,
+                      //   onEditingComplete: () => node.nextFocus(),
+                      // ),
+                      Container(
+                        child: DropdownButton<String>(
                           style: TextStyle(
                             fontFamily: 'Poppins',
-                            color: Colors.white,
                           ),
+                          items: [
+                            DropdownMenuItem<String>(
+                              child: Text(
+                                "Dosen",
+                                style: TextStyle(
+                                  fontFamily: 'Poppins',
+                                  fontSize: 15,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              value: 'one',
+                            ),
+                            DropdownMenuItem<String>(
+                              child: Text(
+                                "Mahasiswa",
+                                style: TextStyle(
+                                  fontFamily: 'Poppins',
+                                  fontSize: 15,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              value: 'two',
+                            ),
+                          ],
+                          onChanged: (String value) {
+                            setState(() {
+                              _value = value;
+                              if (value == "two") {
+                                status = true;
+                              } else {
+                                status = false;
+                              }
+                            });
+                          },
+                          hint: Text('Title'),
+                          value: _value,
                         ),
-                        onPressed: () {},
                       ),
-                    ),
-                  ],
+                      SizedBox(height: 30.0),
+                      Center(
+                        child: RaisedButton(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          color: HexColor("8293c2"),
+                          child: Text(
+                            "Sign Up",
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              color: Colors.white,
+                            ),
+                          ),
+                          onPressed: () async {
+                            if (formKey.currentState.validate()) {
+                              formKey.currentState.save();
+                              print(name);
+                              print(email);
+                              print(password);
+                              print(status);
+                              print(_value);
+                              dynamic result = await auth.regEmailandPass(
+                                  email, password, name, status);
+                            }
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
